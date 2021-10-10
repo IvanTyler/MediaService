63e5265ead31c86f24c5040c0350c846a18738e0
Ivan <Ivan_567.92@mail.ru>
2021-10-11 00:23:35 +0300

    final realise

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,31 @@ $extraOptionsTitle.addEventListener('click', function() {
         $windowExtraOptions.classList.add('hide')
     }
 })
+
+const $nameUser = document.querySelector('#nameUser')
+const $email = document.querySelector('#email')
+
+const $userNameImg = document.querySelector('.form-registration__input-type-img.nameUser')
+const $userNameEmailImg = document.querySelector('.form-registration__input-type-img.email')
+
+$nameUser.addEventListener('input', function() {
+    const lengthStringName = $nameUser.value.trim().length
+    if (lengthStringName >= 1) {
+        $userNameImg.style.opacity = '0'
+        $userNameImg.style.zIndex = '-1'
+    } else {
+        $userNameImg.style.opacity = '1'
+        $userNameImg.style.zIndex = '1'
+    }
+})
+
+$email.addEventListener('input', function() {
+    const lengthStringEmail = $email.value.trim().length
+    if (lengthStringEmail >= 1) {
+        $userNameEmailImg.style.opacity = '0'
+        $userNameEmailImg.style.zIndex = '-1'
+    } else {
+        $userNameEmailImg.style.opacity = '1'
+        $userNameEmailImg.style.zIndex = '1'
+    }
+})
